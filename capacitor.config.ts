827fe1b468679c62ab77490459a71ad12845f4e5
benjamin <133883483+mmmafla@users.noreply.github.com>
2025-07-06 23:32:18 -0400

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'PadPet - Administracion',
-  webDir: 'www'
-};
-
-export default config;
